ef40f25961473737bde1378cfd99b8b4b77c5f11
Alisa Lobanova <17april94@gmail.com>
2022-08-25 20:00:48 +0300

    fixes

--- 1-Drum-kit/index.js
+++ 1-Drum-kit/index.js
@@ -7,14 +7,23 @@ const playSound = (e) => {
     audio.currentTime = 0;
     audio.play();
 }
-
-window.addEventListener('keydown', playSound)
-
 const keys = document.querySelectorAll('.key');
+window.addEventListener('keydown', playSound)
+keys.forEach((key) => key.addEventListener('touchstart', (e) => {
+    e.preventDefault();
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+}, true))
+
 keys.forEach((key) => key.addEventListener('transitionend', (e) => {
     if (e.propertyName !== 'transform') return;
     key.classList.remove('playing')
 }))
 
-keys.forEach((key) => key.addEventListener('touchstart', playSound))
+
 
