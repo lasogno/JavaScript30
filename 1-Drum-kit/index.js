4fc42cdf9e86ec02ff3e10f79c9f6e90bb1122c8
Alisa Lobanova <17april94@gmail.com>
2022-08-25 20:34:20 +0300

    some more fixes

--- 1-Drum-kit/index.js
+++ 1-Drum-kit/index.js
@@ -21,8 +21,19 @@ keys.forEach((tap) => tap.addEventListener('touchstart', (e) => {
     audio.play();
 }))
 
+keys.forEach((key) => key.addEventListener('click', (e) => {
+    e.preventDefault();
+    const audio = document.querySelector(`audio[data-key="${e.currentTarget.dataset.key}"]`);
+    const key = document.querySelector(`div[data-key="${e.currentTarget.dataset.key}"]`);
+    if (!audio) return;
+
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+}))
+
 keys.forEach((key) => key.addEventListener('transitionend', (e) => {
-    if (e.propertyName !== 'transform') return;
+    //if (e.propertyName !== 'transform') return;
     key.classList.remove('playing')
 }))
 
